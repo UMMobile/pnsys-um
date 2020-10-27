e41ff024c7fc6b1b75b6a25cfb7e86b3a538f19c
Jonathan <jonicgp97@gmail.com>
2020-10-27 17:34:25 -0600

    Add docs (example docs)

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "pnsys",
+  "version": "0.0.0",
+  "description": "API for send push notifications",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-10-27T23:20:33.491Z",
+    "url": "http://apidocjs.com",
+    "version": "0.20.1"
+  }
+});
